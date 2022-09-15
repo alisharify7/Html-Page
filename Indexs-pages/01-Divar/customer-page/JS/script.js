3fab39a4af891e024b1fd364d44139d3823e9d00
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2022-09-15 21:51:45 +0430

    Add files via upload

--- Indexs-pages/01-Divar/customer-page/JS/script.js
+++ Indexs-pages/01-Divar/customer-page/JS/script.js
@@ -1,19 +1,3 @@
-var city_tag_names = document.querySelector("#city-tag-names");
-
-
-function switchToggle_changeBgColor(toggle_id) {
-    var toggle = document.querySelector("#" + toggle_id);
-    toggle.classList.toggle("bg-switchToggle-brand");
-}
-
-function sidebarButtons_animation(icon_id) {
-    var icon = document.querySelector("#" + icon_id);
-
-    if (icon.style.transform == "rotateX(180deg)")
-        icon.style.transform = "rotateX(0deg)";
-    else
-        icon.style.transform = "rotateX(180deg)";
-}
 
 function selectCityfromArea(city_id, checkedState) {
     if (city_tag_names.innerHTML.toString().includes("حداقل یک شهر را انتخاب کنید."))
@@ -58,22 +42,3 @@ function unchecked_checkboxes(checkbox_class) {
         checkboxes[i].checked = false;
     }
 }
-
-
-
-// for confirm user in login in header
-const btn_register = document.querySelector("#btn-confirm-login-user");
-const email_form_register_login = document.querySelector("#email-form-register-login");
-
-btn_register.addEventListener('click',()=>{
-    if(email_form_register_login.value.length == 0)
-    {
-        email_form_register_login.style.border = "1px solid red";
-        email_form_register_login.placeholder = "پر کردن این فیلد الزامی است ";
-    }
-    else
-    {
-        email_form_register_login.style.border = "1px solid #eee";
-    }
-})
-// for confirm user in login in header
