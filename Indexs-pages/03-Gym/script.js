f57d9c0241a30fc88b46ee983fa38dde14cd3f8c
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-10-15 22:03:13 +0330

    add animation

--- Indexs-pages/03-Gym/script.js
+++ Indexs-pages/03-Gym/script.js
@@ -10,6 +10,18 @@ window.addEventListener("scroll", e => {
     }
     else {
         document.querySelector("nav").classList.add("nav-menu-top")
-
+        
     }
+    
 })
+window.addEventListener("scroll", e => {
+    if(window.scrollY >= 700){
+        document.querySelector(".award-title").classList.add("fade_right")
+        document.querySelector(".award-image").classList.add("fade_left")
+    }
+    else{
+        document.querySelector(".award-image").classList.remove("fade_left")
+        document.querySelector(".award-title").classList.remove("fade_right")
+    }
+
+});
